3fc21d9f07e38bf934957d1860c1a11d199282dc
Richie Khoo <evolve2k@gmail.com>
2011-06-12 19:25:33 +1000

    Core functionality. Outputs scenario names and step names in colour to debug logger.

--- lib/frogger.rb
+++ lib/frogger.rb
@@ -1,7 +1,2 @@
 module Frogger
-  Before do |scenario|
-      Rails.logger.debug("\n\n #{scenario.name}                                                                                                          (scenario)".black_on_green) 
-  end
 end
-
-World(Frogger)
