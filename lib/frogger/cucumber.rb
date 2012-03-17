87019a168faf5a49f57f791b0add22e2ade4cccf
Scott Harvey <scottandrewharvey@gmail.com>
2012-03-17 11:19:58 +1100

    Removing old files

--- lib/frogger/cucumber.rb
+++
@@ -1,9 +0,0 @@
-Before do |scenario|
-  @frog = Frogger::Frog.new scenario
-  @frog.log_scenario
-  @frog.log_step
-end if respond_to?(:Before)
-
-AfterStep do
-  @frog.log_step
-end if respond_to?(:AfterStep)
